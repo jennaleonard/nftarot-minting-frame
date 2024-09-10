14ccf0dd1541739f73271f5e572933bc562a9732
jennaleonard <jleonardweb@gmail.com>
2024-09-09 21:14:19 -0700

    Code cleanup, added gifs and testing .png in share

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -24,7 +24,7 @@ const randomTokenId = BigInt(randomIndex);
 app.frame("/", (c) => {
   return c.res({
     action: "/card-select",
-    image: "/welcome-img",
+    image: "/nftarot-frame-1.gif",
     intents: [
       <Button value="begin reading">Begin Reading</Button>,
       <Button.Link href="https://www.nftarot.com/about">
@@ -34,107 +34,10 @@ app.frame("/", (c) => {
   });
 });
 
-app.image("/welcome-img", (c) => {
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: "center",
-          background: "black",
-          color: "white",
-          backgroundSize: "100% 100%",
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          flexGap: ".5rem",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-          textWrap: "wrap",
-        }}
-      >
-        <h1
-          style={{
-            textTransform: "uppercase",
-            marginBottom: "0",
-            fontSize: "1.75rem",
-          }}
-        >
-          OnChain Tarot Reading
-        </h1>
-        <p style={{ fontSize: "1.55rem", marginTop: "0" }}>with NFTarot</p>
-        <img
-          src={"/stars1.gif"}
-          alt="stars1"
-          style={{
-            width: "20%",
-            top: "3rem",
-            position: "absolute",
-            left: "22%",
-          }}
-        />
-        <img
-          src={"/card-back.png"}
-          style={{
-            width: "227px",
-            height: "387px",
-            marginBottom: ".25rem",
-          }}
-        />
-        <img
-          src={"/stars2.gif"}
-          alt="stars2"
-          style={{
-            width: "20%",
-            bottom: "6rem",
-            position: "absolute",
-            right: "22%",
-          }}
-        />
-        <p
-          style={{ fontSize: "1.25rem", width: "40%", marginBottom: "3.5rem" }}
-        >
-          Leveraging energy exchange with the blockchain to foster a moment of
-          reflection & guidance.
-        </p>
-      </div>
-    ),
-  });
-});
-
 app.frame("/card-select", (c) => {
   return c.res({
     action: "/card-reveal",
-    image: (
-      <div
-        style={{
-          alignItems: "center",
-          background: "black",
-          backgroundSize: "100%",
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-          color: "white",
-        }}
-      >
-        <img
-          src="/cards_loops_1.gif"
-          alt="floating cards"
-          style={{ margin: "0 auto", width: "70%", height: "130%" }}
-        />
-        <p style={{ position: "absolute", top: "25", right: "28%" }}>
-          Take a deep breath. Set your intention.
-        </p>
-        <p style={{ position: "absolute", bottom: "25", left: "28%" }}>
-          When you're ready, mint your reading.
-        </p>
-      </div>
-    ),
+    image: "/nftarot-frame-2.gif",
     intents: [
       <Button action="/" value="go back">
         Go Back
@@ -174,7 +77,7 @@ app.frame("/card-reveal", async (c) => {
     `Here's my tarot reading for the day from NFTarot:`
   )}&embeds[]=${encodeURIComponent(
     `${process.env.VERCEL_URL}/api/card-reading-${randomTokenId}`
-  )}`;
+  )}.png`;
 
   return c.res({
     image: `/card-reading-${randomTokenId}`,
